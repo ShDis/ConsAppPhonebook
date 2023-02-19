e0487d0bbf06136924dd549f98c008191b9b802c
Dmitriy Sh <theShDis@gmail.com>
2023-02-20 02:02:07 +0400

    ChatGPT code + fixes

---
+++ ConsAppPhonebook/Phonebook.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppPhonebook
+{
+    public class Abonent
+    {
+        public string PhoneNumber { get; set; }
+        public string Name { get; set; }
+    }
+
+    public sealed class Phonebook
+    {
+        private static Phonebook instance = null;
+        private static readonly object padlock = new object();
+        private List<Abonent> abonents = new List<Abonent>();
+        public List<Abonent> Abonents
+        {
+            get { return abonents; }
+        }
+
+        private Phonebook()
+        {
+            ReadFromFile();
+        }
+
+        public static Phonebook Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Phonebook();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void AddAbonent(string phoneNumber, string name)
+        {
+            if (!AbonentExists(phoneNumber))
+            {
+                abonents.Add(new Abonent { PhoneNumber = phoneNumber, Name = name });
+                WriteToFile();
+            }
+        }
+
+        public void DeleteAbonent(string phoneNumber)
+        {
+            for (int i = 0; i < abonents.Count; i++)
+            {
+                if (abonents[i].PhoneNumber == phoneNumber)
+                {
+                    abonents.RemoveAt(i);
+                    WriteToFile();
+                    break;
+                }
+            }
+        }
+
+        public Abonent GetAbonentByPhoneNumber(string phoneNumber)
+        {
+            for (int i = 0; i < abonents.Count; i++)
+            {
+                if (abonents[i].PhoneNumber == phoneNumber)
+                {
+                    return abonents[i];
+                }
+            }
+            return null;
+        }
+
+        public string GetPhoneNumberByName(string name)
+        {
+            for (int i = 0; i < abonents.Count; i++)
+            {
+                if (abonents[i].Name == name)
+                {
+                    return abonents[i].PhoneNumber;
+                }
+            }
+            return null;
+        }
+
+        private void ReadFromFile()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("phonebook.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(',');
+                        if (parts.Length == 2)
+                        {
+                            abonents.Add(new Abonent { PhoneNumber = parts[0], Name = parts[1] });
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading from file: " + ex.Message);
+            }
+        }
+
+        private void WriteToFile()
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("phonebook.txt"))
+                {
+                    foreach (Abonent abonent in abonents)
+                    {
+                        sw.WriteLine(abonent.PhoneNumber + "," + abonent.Name);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error writing to file: " + ex.Message);
+            }
+        }
+
+        private bool AbonentExists(string phoneNumber)
+        {
+            for (int i = 0; i < abonents.Count; i++)
+            {
+                if (abonents[i].PhoneNumber == phoneNumber)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- ConsAppPhonebook/Program.cs
+++ ConsAppPhonebook/Program.cs
@@ -10,6 +10,58 @@ namespace ConsAppPhonebook
     {
         static void Main(string[] args)
         {
+            Phonebook phonebook = Phonebook.Instance;
+
+            //Phonebook p = new Phonebook(); //not working -- everything is okay
+
+            // добавление абонента
+            phonebook.AddAbonent("1234567", "Иванов");
+            phonebook.AddAbonent("7654321", "Петров");
+            phonebook.AddAbonent("8748586", "Emily");
+
+            // получение абонента по номеру телефона
+            Abonent abonent = phonebook.GetAbonentByPhoneNumber("1234567");
+            if (abonent != null)
+            {
+                Console.WriteLine(abonent.Name + ": " + abonent.PhoneNumber);
+            }
+            else
+            {
+                Console.WriteLine("Абонент с таким номером телефона не найден.");
+            }
+
+            // получение номера телефона по имени абонента
+            string phoneNumber = phonebook.GetPhoneNumberByName("Петров");
+            if (phoneNumber != null)
+            {
+                Console.WriteLine("Петров: " + phoneNumber);
+            }
+            else
+            {
+                Console.WriteLine("Абонент с таким именем не найден.");
+            }
+
+            // удаление абонента
+            phonebook.DeleteAbonent("1234567");
+
+            // получение всех абонентов
+            Console.WriteLine("Телефонная книга:");
+            if (phonebook.Abonents != null)
+            {
+                if (phonebook.Abonents.Count != 0)
+                {
+                    foreach (Abonent a in phonebook.Abonents)
+                    {
+                        Console.WriteLine(a.Name + ": " + a.PhoneNumber);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Empty");
+            }
+            
+            Console.ReadLine();
         }
     }
 }
